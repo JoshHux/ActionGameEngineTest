0152bef570150314cf7a30dac8e8f53d0fbc1572
aldie jeon <spirachi77@gmail.com>
2021-10-04 10:30:24 -0600

    json testing

--- Assets/_Project/Scripts/DataParse/SpaxJSONSaver.cs
+++ Assets/_Project/Scripts/DataParse/SpaxJSONSaver.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 using Spax.StateMachine;
 public class SpaxJSONSaver : MonoBehaviour
 {
-    public HitBoxData toSave;
+    public CharacterFrame toSave;
+    public CharacterFrame fromSave;
+
     public string json;
     // Start is called before the first frame update
     void Start()
     {
-        json = JsonUtility.ToJson(toSave);
-        System.IO.File.WriteAllText(Application.persistentDataPath + "/Resources/JSON/TestData/Test.json", json);
+        json = JsonUtility.ToJson(toSave, true);
+        System.IO.File.WriteAllText(Application.persistentDataPath + "/Assets/Resources/JSON/TestData/Test.json", json);
 
+        fromSave = JsonUtility.FromJson<CharacterFrame>(json);
     }
 
     // Update is called once per frame
